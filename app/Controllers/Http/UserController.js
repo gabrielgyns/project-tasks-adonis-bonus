ee2fc88a3d739994944f9184e4b97f3e13f10888
gabriel.soares <gabriel.soares@citsmart.com>
2020-08-13 15:03:51 -0300

    Criação da tabela de endereços de usuários e relacionando com usuários.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -6,8 +6,11 @@ class UserController {
   async store ({ request }) {
     // request.all -> você pegaria todos os dados da request.
     const data = request.only(['username', 'email', 'password'])
+    const addresses = request.input('addresses')
 
     const user = await User.create(data)
+
+    await user.addresses().createMany(addresses)
 
     return user
   }
